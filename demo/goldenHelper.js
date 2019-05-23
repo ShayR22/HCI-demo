914c6af36ce70a8d5199bf4adc0e6b9e9eb7c294
Shay <shayrashinsky@gmail.com>
2019-05-23 12:26:47 +0300

    add goldenHelper

---
+++ demo/goldenHelper.js
@@ -0,0 +1,32 @@
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+})
+
+
+$("#goldenHelperSaveChangesButton").click(function () {
+    var textStyle = extractStringFromSelect("model-font-style-section");
+    var textSize = extractInt("model-font-style-input");
+
+    $('html, body').css('font-family', textStyle);
+    $(".title").css('font-size', textSize * 1.5);
+    $('html, body').css('font-size', textSize);
+    console.log(textStyle);
+    console.log(textSize);
+
+})
+
+
+function extractStringFromSelect(id) {
+    return $("#" + id + " option:selected").val();
+}
+
+function extractInt(id) {
+    var numStr = $("#" + id).val();
+    var number = parseInt(numStr);
+    if (isNaN(number)) {
+        $("#" + id).text("Please enter a number here");
+        return undefined;
+    } else {
+        return number;
+    }
+}
