573cd6e6cd50966c234092a438a053e0ab44a7f3
Liad Khamdadash <liadkh95@gmail.com>
2019-05-28 19:35:24 +0300

    shay dev

--- demo/scripts/song_search.js
+++ demo/scripts/song_search.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
     $("#list-profile-years").click(function () {
+        console.log("fml 2")
         localStorage.setItem("searchBy", "years");
         window.location = "songs-category.html";
     });
 
 
-    $("col-5 center a").click(function () {
+    $("#songCategory a").click(function () {
         console.log("fml");
         var yearVal = $(this).attr("value");
         localStorage.setItem("yearValue", yearVal);
