85e3b23d0af01a5471884c3c806196a27ac4e0de
Liad Khamdadash <liadkh95@gmail.com>
2019-05-28 18:54:10 +0300

    reg

--- demo/scripts/searchAndResultPagesModifier.js
+++ demo/scripts/searchAndResultPagesModifier.js
@@ -26,9 +26,7 @@ $(document).ready(function () {
     }
 
 
-    $("#songSearch").click(function(){
-        document.location="search-songs/songs.html";
-    })
+
 
 })
 
--- demo/scripts/sign.js
+++ demo/scripts/sign.js
@@ -1,17 +1,20 @@
-$("#signupSignup").click(function () {
-    if (!isEmptyData()) {
-        var username = $("#signupUsername").val();
-        var password = $("#signupPassword").val();
-        localStorage.setItem("username", username);
-        localStorage.setItem("password", password);
-
-        console.log(username + " " + password);
-
-        window.location.href = "login.html";
-    } else {
-        alert("please fill username and password");
-    }
-});
+$(document).ready(function(){
+    $("#signupSignup").click(function () {
+        if (!isEmptyData()) {
+            var username = $("#signupUsername").val();
+            var password = $("#signupPassword").val();
+            localStorage.setItem("username", username);
+            localStorage.setItem("password", password);
+    
+            console.log(username + " " + password);
+    
+            window.location.href = "login.html";
+        } else {
+            alert("please fill username and password");
+        }
+    });
+    
+})
 
 function isEmptyData() {
     var usernameInput = $("#signupUsername").val();
