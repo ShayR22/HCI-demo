f21470a1b05e36009e30a87c0e89dd299dac4f9a
Liad Khamdadash <liadkh95@gmail.com>
2019-06-05 12:52:24 +0300

    Fix bugs

--- demo/scripts/result.js
+++ demo/scripts/result.js
@@ -21,35 +21,24 @@ function generateData(searchFor, searchCategory, searchSubCategory) {
 
     $("#dataGenerated").html("");
     $("#dataGenerated").html(" \
-    <nav aria-label='breadcrumb'> \
-    <ol class='breadcrumb'> \
-      <li class='breadcrumb-item active' aria-current='page'> \
-      " + searchFor + " / " + searchCategory + " / " + searchSubCategory + " </li> \
-    </ol> \
-  </nav> \
     <div class='list-group' id='resultGenereated'> \
     <a href='#' class='list-group-item list-group-item-action'> \
         <div class='d-flex w-100 justify-content-between'> \
-            <h5 class='mb-1'>List group item heading</h5> \
+            <h5 class='mb-1'>Name</h5> \
         </div>\
-        <p class='mb-1'>Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius \
-            blandit.</p> \
-        <small>Donec id elit non mi porta.</small> \
+        <p class='mb-1'>Details</p> \
     </a> \
     <a href='#' class='list-group-item list-group-item-action'> \
         <div class='d-flex w-100 justify-content-between'> \
-            <h5 class='mb-1'>List group item heading</h5> \
+            <h5 class='mb-1'>Name</h5> \
         </div> \
-        <p class='mb-1'>Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius \
-            blandit.</p> \
+        <p class='mb-1'>Details</p> \
     </a> \
     <a href='#' class='list-group-item list-group-item-action'> \
         <div class='d-flex w-100 justify-content-between'> \
-            <h5 class='mb-1'>List group item heading</h5> \
+            <h5 class='mb-1'>Name</h5> \
         </div> \
-        <p class='mb-1'>Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius \
-            blandit.</p> \
-        <small class='text-muted'>Donec id elit non mi porta.</small> \
+        <p class='mb-1'>Details</p> \
     </a> \
 </div> \
 </div> \
